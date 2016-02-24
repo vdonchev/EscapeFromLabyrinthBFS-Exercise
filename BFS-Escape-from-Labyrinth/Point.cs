64a988b311695da5617bc8050b1201127176b984
vdonchev <ya.5164646777752015@yandex.ru>
2016-02-24 11:27:32 +0200

    init commit

---
+++ BFS-Escape-from-Labyrinth/Point.cs
@@ -0,0 +1,31 @@
+﻿namespace Escape_from_Labyrinth
+{
+    public class Point
+    {
+//        public Point(int x, int y)
+//        {
+//            this.X = x;
+//            this.Y = y;
+//        }
+//
+//        public Point(
+//            int x, 
+//            int y, 
+//            string direction, 
+//            Point previousPoint)
+//            : this(x, y)
+//        {
+//            this.Direction = direction;
+//            this.PreviousPoint = previousPoint;
+//        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public string Direction { get; set; }
+
+        public Point PreviousPoint { get; set; }
+         
+    }
+}
